83a1f4fe3df789d965fcd20a73da124dca7965cb
Code-Phantom-Thief <code.phantom.thief@gmail.com>
2021-09-16 14:15:48 +0900

    mern todo-app

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,4 +1,5 @@
-export const url = 'http://localhost:5000/api';
+// export const url = 'http://localhost:5000/api';
+export const url = 'https://mern-project-todoapp.herokuapp.com/api';
 export const setHeaders = () => {
     const header = {
         headers: {
